59844f3962af57afb0907896cb986cc2572ddb5e
Daniel <danielnp525@gmail.com>
2016-08-05 14:49:57 -0400

    Fixed javascript, started character pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,27 @@
 $(function () {
     var body = $("body");
+    var nav = body.find(">header>nav");
     var tabs = body.find("nav-tabs");
+    var offset= -120;
+    var characters = body.find("#characters");
+    var charModal = body.find("#char-modal-backdrop");
 
     tabs.find("li a")
         .click(function (e) {
             e.preventDefault()
             $(this).tab('show')
+        });
+
+    nav.find("li a")
+        .linkScroller({ 
+            offset: offset
+        })
+        .scrollNav({
+            offset: offset
+        });
+
+    characters.find(".row .char-section .char-block")
+        .popupModal({
+            modal: charModal
         });
 });
